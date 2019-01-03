77d04a7885595919f0626b6da49b1db2118194f0
Lokesh Kumar jain <lokeshjain2008@gmail.com>
2019-01-03 15:51:13 +0530

    added changes to show contact us and removed clients for now

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -5,57 +5,40 @@
 */
 
 
-(function($) {
+(function ($) {
 
-   //Random Text
-
-	var messages=['Website Development','Mobile App Development','Web App Development' , 'Ionic App Development ' , 'Angular App Development' , 'React App Development' ];
-	var rank=1;
-	document.getElementById('myTypewriter').addEventListener('webkitAnimationEnd', changeTxt);
-	document.getElementById('myTypewriter').addEventListener('animationend', changeTxt);
-	document.getElementById('mobileTypewriter').addEventListener('webkitAnimationEnd', changeTxt);
-	document.getElementById('mobileTypewriter').addEventListener('animationend', changeTxt);
-	function changeTxt(e){
-		console.log(this) ;
-	  var _h1 = this.getElementsByTagName('h1') ;
-console.log(_h1[0].style);
-	  _h1[0].style.WebkitAnimation = 'none'; // set element animation to none
-	   setTimeout(function() { // you surely want a delay before the next message appears
-		  _h1[0].innerHTML=messages[rank];
-		  var speed =3.5*messages[rank].length/50; // adjust the speed (3.5 is the original speed, 20 is the original string length
-		  _h1[0].style.WebkitAnimation = 'typing '+speed+'s steps(40, end), blink-caret .75s step-end infinite'; //  switch to the original set of animation      
-		  (rank===messages.length-1)?rank=0:rank++; // if you have displayed the last message from the array, go back to the first one, else go to next message
-		}, 1000);
-	}
-})(jQuery);
-
-//Scrolling Header Cbange function 
-window.onscroll = () => {
-	var windowObj = window ; 
-	const nav = document.querySelector('#navbar');
-	console.log(nav);
-	if(windowObj.scrollY <= 700) nav.className = 'navbar navbar-expand-lg fixed-top nav bg-secondary'; else nav.className = 'navbar nav-op navbar-expand-lg fixed-top nav bg-secondary';
-  };
-
-  $('.filter-button').click(function(){
-	var value = $(this).attr('data-filter');
-	
-	if(value == 'all')
-	{
-		//$('.filter').removeClass('hidden');
-		$('.filter').show('1000');
-	}
-	else
-	{
-//            $('.filter[filter-item="'+value+'"]').removeClass('hidden');
-//            $(".filter").not('.filter[filter-item="'+value+'"]').addClass('hidden');
-		$('.filter').not('.'+value).hide('3000');
-		$('.filter').filter('.'+value).show('3000');
-		
-	}
+  //Scrolling Header Cbange function
+$(window).scroll( () => {
+  var windowObj = window;
+  const nav = $('#navbar');
+  if (windowObj.scrollY <= 500) {
+    nav.addClass('transparent');
+  }else{
+    nav.removeClass('transparent');
+  }
 });
 
-if ($('.filter-button').removeClass('active')) {
-$(this).removeClass('active');
-}
-$(this).addClass('active');
+  //Random Text
+
+  var messages = ['Website Development', 'Mobile App Development', 'Web App Development', 'Ionic App Development ', 'Angular App Development', 'React App Development'];
+  var rank = 1;
+  document.getElementById('myTypewriter').addEventListener('webkitAnimationEnd', changeTxt);
+  document.getElementById('myTypewriter').addEventListener('animationend', changeTxt);
+  document.getElementById('mobileTypewriter').addEventListener('webkitAnimationEnd', changeTxt);
+  document.getElementById('mobileTypewriter').addEventListener('animationend', changeTxt);
+
+  function changeTxt(e) {
+    console.log(this);
+    var _h1 = this.getElementsByTagName('h1');
+    console.log(_h1[0].style);
+    _h1[0].style.WebkitAnimation = 'none'; // set element animation to none
+    setTimeout(function () { // you surely want a delay before the next message appears
+      _h1[0].innerHTML = messages[rank];
+      var speed = 3.5 * messages[rank].length / 50; // adjust the speed (3.5 is the original speed, 20 is the original string length
+      _h1[0].style.WebkitAnimation = 'typing ' + speed + 's steps(40, end), blink-caret .75s step-end infinite'; //  switch to the original set of animation
+      (rank === messages.length - 1) ? rank = 0: rank++; // if you have displayed the last message from the array, go back to the first one, else go to next message
+    }, 1000);
+  }
+})(jQuery);
+
+
